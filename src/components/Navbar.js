b43ec40a0e63d8cedd638adafcbbc60cf9abe77d
AlenaAlyona <izakson91@gmail.com>
2021-11-28 18:14:33 +0100

    task: devide navbar into 3 sections: left, middle and right

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,31 +6,39 @@ import Cart from "../assets/cart-icon.svg";
 function Navbar() {
   return (
     <div className="navbar">
-      <div className="hamburger-menu">
-        <div></div>
-        <div></div>
-        <div></div>
+      <div className="navbar-left">
+        <div className="hamburger-menu">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="close-btn" id="close"></div>
+        <a href="#home">
+          <img className="logo" src={Logo} alt="STOX logo" />
+        </a>
       </div>
-      <div className="close-btn" id="close"></div>
-      <a href="#home">
-        <img className="logo" src={Logo} alt="STOX logo" />
-      </a>
-      <a href="#everyday">Everyday</a>
-      <a href="#sports">Sports</a>
-      <a href="#travel">Travel</a>
-      <a href="#medical">Medical</a>
-      <a href="#ambassadors">Ambassadors</a>
-      <a href="#technology">Technology</a>
-      <a href="#reviews">Reviews</a>
-      <a href="#help">Help</a>
-      <a href="#account">Account</a>
-      <img src={Search} className="icon" alt="search icon"></img>
-      <img
-        src={Cart}
-        fill="FFF"
-        className="icon"
-        alt="shopping cart icon"
-      ></img>
+
+      <div className="navbar-middle">
+        <a href="#everyday">Everyday</a>
+        <a href="#sports">Sports</a>
+        <a href="#travel">Travel</a>
+        <a href="#medical">Medical</a>
+      </div>
+
+      <div className="navbar-right">
+        <a href="#ambassadors">Ambassadors</a>
+        <a href="#technology">Technology</a>
+        <a href="#reviews">Reviews</a>
+        <a href="#help">Help</a>
+        <a href="#account">Account</a>
+        <img src={Search} className="icon" alt="search icon"></img>
+        <img
+          src={Cart}
+          fill="FFF"
+          className="icon"
+          alt="shopping cart icon"
+        ></img>
+      </div>
     </div>
   );
 }
