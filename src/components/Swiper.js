ea32f63d2caa2e3183dae2bc4cb01d807b2eba10
AlenaAlyona <izakson91@gmail.com>
2021-11-29 04:30:55 +0100

    fix: add gradient to swiper img

--- src/components/Swiper.js
+++ src/components/Swiper.js
@@ -5,24 +5,31 @@ function Swiper() {
     <div className="swiper-wrapper">
       <div className="swiper">
         <div className="item" title="Everyday">
+          <div className="gradient"></div>
           <div className="item-inner one"></div>
         </div>
         <div className="item" title="Sports">
+          <div className="gradient"></div>
           <div className="item-inner two"></div>
         </div>
         <div className="item" title="Skiing">
+          <div className="gradient"></div>
           <div className="item-inner three"></div>
         </div>
         <div className="item" title="Travel">
+          <div className="gradient"></div>
           <div className="item-inner four"></div>
         </div>
         <div className="item" title="Recovery">
+          <div className="gradient"></div>
           <div className="item-inner five"></div>
         </div>
         <div className="item" title="Hiking">
+          <div className="gradient"></div>
           <div className="item-inner six"></div>
         </div>
         <div className="item" title="Medical">
+          <div className="gradient"></div>
           <div className="item-inner seven"></div>
         </div>
       </div>
